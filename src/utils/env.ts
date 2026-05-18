9882ddc69046a4ed92a3f4d4964d6a4f93bde8b3
Sukhpal Singh <sukhpalmail@protonmail.com>
2026-05-18 21:25:25 +1000

    Feature: Added faster approach for audio streaming

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,6 @@
 export const ENV = {
   environment: import.meta.env.VITE_ENV,
+  wssApiUrl: import.meta.env.VITE_NODE_API_URL,
   apiUrl: import.meta.env.VITE_API_URL,
   isDev: import.meta.env.VITE_ENV === "dev",
   sentryDsn: import.meta.env.VITE_SENTRY_DSN,
